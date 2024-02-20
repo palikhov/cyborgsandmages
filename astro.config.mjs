93ee1e42b776640de64f9e3c32228884a2c3c76d
Aleksandr Vasilenko <phenomen@pm.me>
2024-02-20 15:51:48 -0600

    Keystatic and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 import Icons from 'unplugin-icons/vite';
+import keystatic from '@keystatic/astro';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
@@ -19,6 +22,7 @@ export default defineConfig({
 	}),
 	integrations: [
 		svelte(),
+		react(),
 		mdx({
 			remarkRehype: {
 				footnoteLabel: 'Примечания'
@@ -37,7 +41,13 @@ export default defineConfig({
 			syntaxHighlight: false
 		}),
 		tailwind(),
-		sitemap()
+		keystatic(),
+		sitemap({
+			filter: (page) =>
+				page !== 'https://cyborgsandmages.com/keystatic' && page !== 'https://cyborgsandmages.com/keystatic/'
+		}),
+		...(process.env.ENABLE_KEYSTATIC ? [keystatic()] : []),
+		,
 	],
 	vite: {
 		plugins: [
