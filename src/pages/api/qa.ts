fac0f425d7ca312662d5b35d5e9ae190462e14d8
Aleksandr Vasilenko <phenomen@pm.me>
2024-07-11 13:16:42 -0500

    Update qa.ts

--- src/pages/api/qa.ts
+++ src/pages/api/qa.ts
@@ -7,21 +7,20 @@ import { createOpenAI } from '@ai-sdk/openai';
 import { StreamingTextResponse, streamText } from 'ai';
 
 export const POST: APIRoute = async ({ request }) => {
-  const groq = createOpenAI({
-    apiKey: GROQ_KEY,
-    baseURL: "https://api.groq.com/openai/v1",
-  });
-  
-  const { prompt } = await request.json();
+	const groq = createOpenAI({
+		apiKey: GROQ_KEY,
+		baseURL: "https://api.groq.com/openai/v1",
+	});
+
+	const { prompt } = await request.json();
 
 	const result = await streamText({
 		model: groq("llama3-8b-8192"),
-		temperature: 0,
-		maxTokens: 2000,
+		maxTokens: 5000,
 		system:
 			"You're an assistant that summarizes the content of the provided article. Always reply in Russian language.",
 		prompt: `Article: ${prompt}`,
 	});
 
-	return new StreamingTextResponse(result.toAIStream()); 
+	return new StreamingTextResponse(result.toAIStream());
 };
