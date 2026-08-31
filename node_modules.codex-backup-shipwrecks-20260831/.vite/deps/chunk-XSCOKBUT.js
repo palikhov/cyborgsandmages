71c974d108dab0da3f529cba79e1b5522802f3da
Anton Palikhov <a.v.palikhov@hotmail.com>
2026-08-31 04:53:33 +0300

    1

--- node_modules.codex-backup-shipwrecks-20260831/.vite/deps/chunk-XSCOKBUT.js
+++
@@ -1,9 +0,0 @@
-// node_modules/.pnpm/svelte@4.2.15/node_modules/svelte/src/shared/version.js
-var VERSION = "4.2.15";
-var PUBLIC_VERSION = "4";
-
-export {
-  VERSION,
-  PUBLIC_VERSION
-};
-//# sourceMappingURL=chunk-XSCOKBUT.js.map
